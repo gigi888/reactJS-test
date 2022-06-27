cfbedfc8599c661640ba316752bddf35216405c3
gcheng <gcheng@constantcontact.com>
2022-06-27 17:22:55 -0400

    enable CORS

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const express = require("express");
+  const cors = require("cors");
+  const app = express();
+//use cors as middleware
+  app.use(cors())
+
+  app.use(
+      cors({
+        origin: "http://localhost:3000",
+      credentials: true
+   })
+  )
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CORS Enabled. Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
